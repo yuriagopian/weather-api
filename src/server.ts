db0654c8c3ab1a432df3d9163e282d5888d48bc7
yuriagopian <yuri.agopian@hotmail.com>
2022-12-21 15:30:27 -0300

    chore: add server initial config

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import { Server } from "@overnightjs/core";
+import bodyParser from "body-parser";
+import "./util/module-alias";
+
+export class SetupServer extends Server {
+  constructor(private port = 3000) {
+    super();
+  }
+
+  public init(): void {
+    this.setupExpress();
+  }
+
+  private setupExpress(): void {
+    this.app.use(bodyParser.json());
+  }
+}
