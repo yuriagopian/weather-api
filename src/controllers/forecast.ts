d366ad8cd90fa54d529b2179e16d183064e118aa
yuriagopian <yuri.agopian@hotmail.com>
2022-12-21 16:16:57 -0300

    chore: add forecast initial controller

---
+++ src/controllers/forecast.ts
@@ -0,0 +1,8 @@
+import { Controller, Get } from "@overnightjs/core";
+import { Request, Response } from "express";
+
+@Controller("forecast")
+export class ForecastController {
+  @Get("")
+  public getForecastForLoggedUser(_: Request, res: Response): void {}
+}
